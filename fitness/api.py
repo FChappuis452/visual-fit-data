057b5ecbcca484734becee0322b5b7bfffb59b3b
Frederic Chappuis <fchappuis452@gmail.com>
2020-08-20 20:10:16 -0400

    removed files created API class

---
+++ fitness/api.py
@@ -0,0 +1,80 @@
+"""
+This class will make the API queries and return the raw data to the view
+"""
+
+
+"""
+access_token = SocialToken.objects.get(account__user=request.user, account__provider='google')
+    refresh_token(access_token)
+
+    url = "https://www.googleapis.com/fitness/v1/users/me/dataset:aggregate"
+    headers = {
+        'Content-type' : 'application/json',
+        'Authorization' : 'Bearer ' + str(access_token),
+    }
+    
+    body = {
+        'aggregateBy' : [{'dataSourceId' : 'derived:com.google.step_count.delta:com.google.android.gms:estimated_steps'}],
+        "bucketByTime" : { "durationMillis" : 86400000 },
+        "startTimeMillis" : 1593921600000,
+        "endTimeMillis" : 1594094399000
+    }
+
+    response = requests.post(url, json=body, headers=headers)
+    reply = response.json()
+    
+
+"""
+import os
+import requests
+
+from allauth.socialaccount.models import SocialToken
+
+class ApiCalls():
+
+    def __init__(self, request):
+        self.access_token = SocialToken.objects.get(account__user=request.user, account__provider='google')
+        self.token_secret = self.access_token.token_secret
+    
+
+    def test_call(self):
+        url = "https://www.googleapis.com/fitness/v1/users/me/dataset:aggregate"
+        headers = {
+            'Content-type' : 'application/json',
+            'Authorization' : 'Bearer ' + str(self.access_token),
+        }
+        
+        body = {
+            'aggregateBy' : [{'dataSourceId' : 'derived:com.google.step_count.delta:com.google.android.gms:estimated_steps'}],
+            "bucketByTime" : { "durationMillis" : 86400000 },
+            "startTimeMillis" : 1593921600000,
+            "endTimeMillis" : 1594094399000
+        }
+
+        response = requests.post(url, json=body, headers=headers)
+        
+        return response.json()
+
+
+    
+    
+    def refresh_token(self, request):
+        client_secret = os.environ['CLIENT_SECRET']
+        client_id = os.environ['CLIENT_ID']
+        url = 'https://oauth2.googleapis.com/token'
+
+        headers = {
+            'content-type' : 'application/x-www-form-urlencoded',
+        }
+        
+        body = {
+            'client_secret' : client_secret,
+            'grant_type' : 'refresh_token',
+            'refresh_token' : self.token_secret,
+            'client_id' : client_id
+        }
+            # f"client_secret={client_secret}&grant_type=refresh_token&refresh_token={self.token_secret}&client_id={client_id}"
+        
+        
+        response = requests.post(url, json=body)
+        return response.json()
--- fitness/views.py
+++ fitness/views.py
@@ -4,49 +4,35 @@ import requests
 from allauth.socialaccount.models import SocialToken
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-
+from .api import ApiCalls
 #from oauthlib.oauth2 import BackendApplicationClient
 
 
 
 @login_required()
 def fitness_home(request):
-    access_token = SocialToken.objects.get(account__user=request.user, account__provider='google')
-    refresh_token(access_token)
 
-    url = "https://www.googleapis.com/fitness/v1/users/me/dataset:aggregate"
-    headers = {
-        'Content-type' : 'application/json',
-        'Authorization' : 'Bearer ' + str(access_token),
-    }
-    
-    body = {
-        'aggregateBy' : [{'dataSourceId' : 'derived:com.google.step_count.delta:com.google.android.gms:estimated_steps'}],
-        "bucketByTime" : { "durationMillis" : 86400000 },
-        "startTimeMillis" : 1593921600000,
-        "endTimeMillis" : 1594094399000
-    }
-
-    response = requests.post(url, json=body, headers=headers)
-    reply = response.json()
-    print(reply)
+    api = ApiCalls(request)
+    r= api.refresh_token(request)
+        
+    reply = api.test_call()    
 
     return render(request, 'fitness/fitness.html',{
         'fitdata' : reply,
-        'token' : access_token
+        'token' : r
     })
 
-def refresh_token(token):
-    url = 'https://oauth2.googleapis.com/token'
-    client_secret = os.environ['CLIENT_SECRET']
-    client_id = os.environ['CLIENT_ID']
-    r_token = token.token_secret
+# def refresh_token(token):
+#     url = 'https://oauth2.googleapis.com/token'
+#     client_secret = os.environ['CLIENT_SECRET']
+#     client_id = os.environ['CLIENT_ID']
+#     r_token = token.token_secret
 
-    headers = {
-        'content-type' : 'application/x-www-form-urlencoded',
-    }
+#     headers = {
+#         'content-type' : 'application/x-www-form-urlencoded',
+#     }
     
-    body = f"client_secret={client_secret}&grant_type=refresh_token&refresh_token={r_token}&client_id={client_id}"
+#     body = f"client_secret={client_secret}&grant_type=refresh_token&refresh_token={r_token}&client_id={client_id}"
 
     
-    refresh = requests.post(url, data=body)
+#     refresh = requests.post(url, data=body)
