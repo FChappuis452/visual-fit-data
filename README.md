dd87c3b76d93a46de7e5201ec8de7c5e70e14ca4
Frederic Chappuis <fchappuis452@gmail.com>
2020-09-09 08:08:32 -0400

    added to readme, styled output to HTML page.

--- README.md
+++ README.md
@@ -3,6 +3,10 @@ In this project I will use the Django framework, and google's Fitness API to hel
 
 It will feature having to login into google to authenticate yourself and then it will pull the data from the API.  
 
+Some limitiation that the API has is that it can only retrieve 90 days worth of data.
+
 It is currently still under construction and is a live version is hosted on https://visual-fit-data.herokuapp.com/fitness/
 Due to the limitation of a free heroku account, the page might take a few seconds to become responsive. This is because the free version of 
 heroku "powers down" when not being actively used.
+
+
--- fitness/views.py
+++ fitness/views.py
@@ -7,7 +7,7 @@ from allauth.socialaccount.models import SocialToken
 from .api import ApiCalls
 from .date_converter import DateConverter
 from datetime import date, datetime, timedelta
-from dateutil.relativedelta import relativedelta
+from dateutil.parser import parse
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from .extract import *
@@ -28,34 +28,36 @@ def fitness_home(request):
         'fitness_info' : "",
         'date_count' : "",
         'fitness_count' : "",
+        'fitness_total' : "",
+        'fitness_avg' : "",
     }
     
-    endDate = datetime.now().replace(hour=0, minute=0, second=0)
+    endDate = datetime.now().replace(hour=23, minute=59, second=59)
     
     if request.method == 'POST':
         form = request.POST
         # always hate that python does not have switch statements
         if 'getData' in form:   
-            startDate = form['startDate']
-            endDate = form['endDate']
+            # https://stackoverflow.com/questions/25341945/check-if-string-has-date-any-format
+            
+            startDate = is_date(form['startDate'])
+            endDate = is_date(form['endDate'])
 
+            
+            
             if startDate == "":
                 startDate = dt.datetime.now().replace(hour=0, minute=0, second=0)
             if endDate == "":
                 endDate = dt.datetime.now().replace(hour=0, minute=0, second=0)
 
-        elif 'fourWeeks' in form:
-            startDate = endDate - timedelta(weeks=4)
+        elif 'thirtyDays' in form:
+            startDate = endDate - timedelta(days=29)
             
-        elif 'threeMonth' in form:
-            startDate = endDate + relativedelta(months=-3)
+        elif 'sixtyDays' in form:
+            startDate = endDate - timedelta(days=59)
             
-        elif 'sixMonth' in form:
-            startDate = endDate + relativedelta(months=-6)
-            
-        elif 'twelveMonth' in form:
-            startDate = endDate + relativedelta(months=-12)
-            
+        elif 'ninetyDays' in form:
+            startDate = endDate - timedelta(days=89)
     else:
         startDate = endDate
 
@@ -65,42 +67,57 @@ def fitness_home(request):
         
     if type(endDate) is str:
         endDate = dt.datetime.strptime(endDate, '%a %b %d, %Y')
+
+    # check if selected date is greater than 90 days
+    startDate.replace(hour=0, minute=0, second=0)
+    delta = endDate - startDate
+   
+    if (delta.days) >= 90:
+        endDate = startDate + timedelta(days=89)
         
     reply = api.get_data(startDate, endDate)            
 
     fields = json_extract(reply)
     
    
-    fields['date_count'] = len(fields["dates"])
-    fields['fitness_count'] = len(fields["fitness_data"])
-
+    
     #convert dates
     converter = DateConverter()
     for idx, milli_date in enumerate(fields['dates']):
         fields['dates'][idx] = converter.convert_to_date(milli_date)
     
-    
-       
+    # setting up all the data to send back to the template
+    fields['date_count'] = len(fields["dates"])
+    fields['fitness_count'] = len(fields["fitness_data"])
+    fields['fitness_total'] = sum(fields['fitness_data'])
+    fields['fitness_avg'] = fields['fitness_total'] / fields['fitness_count']
+    zippy = zip(fields['dates'], fields['fitness_data']) #combine the two lists
+    zippy = list(zippy)
+    for i,j in zippy:
+        print(i,j)
 
     return render(request, 'fitness/fitness.html',{
-        'dates' : fields["dates"],
-        'steps' : fields["fitness_data"],
+        "zipped_data" : zippy,
         'd_count' : fields["date_count"],
         's_count' : fields["fitness_count"],
+        'total_count' : fields["fitness_total"],
+        'average' : fields["fitness_avg"]
         
     })
 
-# def refresh_token(token):
-#     url = 'https://oauth2.googleapis.com/token'
-#     client_secret = os.environ['CLIENT_SECRET']
-#     client_id = os.environ['CLIENT_ID']
-#     r_token = token.token_secret
 
-#     headers = {
-#         'content-type' : 'application/x-www-form-urlencoded',
-#     }
-    
-#     body = f"client_secret={client_secret}&grant_type=refresh_token&refresh_token={r_token}&client_id={client_id}"
 
-    
-#     refresh = requests.post(url, data=body)
+
+def is_date(string, fuzzy=False):
+    """
+    Return whether the string can be interpreted as a date.
+
+    :param string: str, string to check for date
+    :param fuzzy: bool, ignore unknown tokens in string if True
+    """
+    try: 
+        parse(string, fuzzy=fuzzy)
+        return string
+
+    except ValueError:
+        return ""
